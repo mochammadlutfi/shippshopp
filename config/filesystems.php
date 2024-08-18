907ee5afbdc89dc163ce0e5e8769c86277e5e6aa
Mochammad Lutfi <mochammadlutfi125@gmail.com>
2024-08-19 01:07:11 +0700

    fx

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path(),
+            'root' => public_path('/uploads'),
             'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
